acb9104364a991d98bb6e864bed641617f0694dd
shreyavod <shreyav@brightcomgroup.com>
2023-01-02 04:48:22 -0800

    task9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def uppercase(str):
-    """Print a string in uppercase."""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("")
+def print_last_digit(number):
+    if number >= 0:
+        last_digit = number % 10
+    elif number < 0:
+        last_digit = (number * -1) % 10
+    print(last_digit, end='')
+    return last_digit
