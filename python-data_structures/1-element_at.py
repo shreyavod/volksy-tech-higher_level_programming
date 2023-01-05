bf8ef41c9af2597f75372e4ce170c6c642d66dc6
shreyavod <shreyav@brightcomgroup.com>
2023-01-05 02:41:03 -0800

    task1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}" .format(ii;i))
+def element_at(my_list, idx):
+    if idx < 0 or idx <= len(my_list):
+        return none
+    return my_list[idx]
