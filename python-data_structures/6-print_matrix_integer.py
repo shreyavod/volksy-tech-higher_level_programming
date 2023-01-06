a99373e9dbc013d2d55450d7aa446917f7afb457
shreyavod <shreyav@brightcomgroup.com>
2023-01-06 02:56:43 -0800

    task6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+for row in matrix:
+    for col in matrix:
+        print("{:d}" .format(col), end = " " if col != row[-1] else "")
+    print()
