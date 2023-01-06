ff483d1be5e2e2c0c6e4662b586664f7b6bcbf08
shreyavod <shreyav@brightcomgroup.com>
2023-01-06 02:20:22 -0800

    task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    copy = my_list.copy()
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list.copy()
-    else:
-        copy[idx] = element
-        return copy
+def no_c(my_string):
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
