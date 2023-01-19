644a96191368290d437aef3843b76d7a7f8773bd
shreyavod <shreyav@brightcomgroup.com>
2023-01-19 03:53:17 -0800

    t2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write a file"""
+
+
+def append_file(filename="", text=""):
+    """write"""
+    if filename:
+        with open(filename, mode='a', encoding="utf-8") as f:
+            return (f.write(text))
