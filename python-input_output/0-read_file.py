02ada8a41252835d2ea40281a8e294b7a6f6f911
shreyavod <shreyav@brightcomgroup.com>
2023-01-19 21:36:04 -0800

    t0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""zero task in input/output
-"""
+# 0-read_file.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """functions only
-    """
-   
-with open(filename, encoding="utf-8") as readFile:
-    print(readFile.read(),end='')
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
