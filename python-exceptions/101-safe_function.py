fb4177b8aecfb283b91c57a8ccadffa8d2bce509
shreyavod <shreyav@brightcomgroup.com>
2023-02-08 04:24:53 -0800

    task101

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Function add_attribute
-"""
-
-
-def add_attribute(a_class, name, value):
-    """Adds new attribute to an object if it's possible
-    """
-
-    # Set for O(1) membership test
-    cannot_add = {int, str, float, list, dict, tuple, frozenset, type, object}
-
-    if type(a_class) in cannot_add:
-        raise TypeError("can't add new attribute")
-
-    a_class.__setattr__(name, value)
+def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return
+    return (result)
