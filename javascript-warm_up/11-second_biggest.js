5504e441a04fac8b009df24e64aabc61d8676e7f
shreyavod <shreyav@brightcomgroup.com>
2023-02-22 20:49:35 -0800

    al

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
-  return parseInt(x);
-});
-
-if (args.length <= 1) {
+const array = process.argv.slice(2);
+let array2 = [];
+function sortNumber (a, b) {
+  return a - b;
+}
+if (array[0] === undefined) {
+  console.log(0);
+} else if (array.length === 1) {
   console.log(0);
 } else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+  array2 = array.sort(sortNumber);
+  array2.pop();
+  console.log(parseInt(array2[array2.length - 1]));
 }
