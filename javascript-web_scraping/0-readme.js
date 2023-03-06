58d638283836bca9b5b75e445aa5e133fd027e2c
shreyavod <shreyav@brightcomgroup.com>
2023-03-06 10:20:24 +0000

    all

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-module.exports = {
-  Rectangle: function () {}
-};
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', errorFunc);
+
+function errorFunc (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+}
